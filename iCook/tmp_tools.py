81e5371c9272408bde18290873a5925c9872d57f
Michael <wolf@bad-wolf.fr>
2014-05-12 10:10:44 +0200

    modified:   iCook/tmp_tools.py

--- iCook/tmp_tools.py
+++ iCook/tmp_tools.py
@@ -1,4 +1,5 @@
 import sqlite3
+import uuid
 #tools to correct db errors
 
 conn = sqlite3.connect("recipeBDD",detect_types= sqlite3.PARSE_COLNAMES)
@@ -7,6 +8,8 @@ c.execute("""SELECT pictureLocation FROM onlineRecipe""")
 result = c.fetchall()
 for i in result:
     print(i[0][:i[0].rfind(".")]+".gif",i[0])
-    c.execute("""update onlineRecipe set pictureLocation = ? where pictureLocation = ?""",[i[0][:i[0].rfind(".")]+".gif",i[0]])
+    c.execute("""update onlineRecipe set pictureLocation = ?, recipeID = ?, isFav=False where pictureLocation = ?""",[i[0][:i[0].rfind(".")]+".gif",uuid.uuid4().hex,i[0]])
 conn.commit()
+
+
 c.close()
