9489f4e74a4b6bc1e78988d073b702af4de9a3e9
Michael <wolf@bad-wolf.fr>
2014-05-20 10:49:33 +0200

    now support recipe image (done in an horrible way)

--- iCook/Frame/RecipeFrame.py
+++ iCook/Frame/RecipeFrame.py
@@ -12,6 +12,11 @@ class RecipeFrame(Frame.Frame):
 
     def __init__(self,parent=None,mainFrame=None,**kwarg):
         Frame.Frame.__init__(self,parent,mainFrame,padding=5,**kwarg)
+
+        self.columnconfigure(0, weight=1)
+        self.rowconfigure(1, weight=1)
+
+
         self.recipe = Recipe.Recipe()
         self.pic = None
         self.editButton = ttk.Button(self, text="Edit", command=self.editRecipe)
@@ -19,16 +24,23 @@ class RecipeFrame(Frame.Frame):
 
         self.favChoice = tk.IntVar()
         self.favButton = ttk.Checkbutton(self, text="Ajouter aux favoris",variable=self.favChoice,command=self.addToFav)
-        self.favButton.pack(side=tk.TOP)
+        # self.favButton.pack(side=tk.TOP)
+        self.favButton.grid(row=0,column=0,sticky=tk.E+tk.W)
 
 
         self.textContent = tk.Text(self,state="disable")
         self.textContent.tag_config("header",font=font.Font(size=22),justify="center")
-        self.textContent.tag_config("header2",font=font.Font(size=18),justify="center")
+        self.textContent.tag_config("header2",font=font.Font(size=18),justify="center")        
+        self.textContent.tag_config("pic",justify="right")
 
         self.textContent.tag_config("list",lmargin1=20,lmargin2=20)
 
-        self.textContent.pack(fill=tk.BOTH, expand=1)
+        # self.textContent.pack(fill=tk.BOTH, expand=1)
+        self.textContent.grid(row=1,column=0,sticky=tk.E+tk.W+tk.S+tk.N)
+
+        self.picLabel = tk.Label(self)
+        self.picLabel.grid(row=1,column=0,sticky=tk.E+tk.N,padx=(5,5),pady=(5,5))
+
 
 
     def editRecipe(self):
@@ -42,7 +54,7 @@ class RecipeFrame(Frame.Frame):
         # try:
         print(os.path.dirname(os.path.realpath(__file__))+"/../res/pic/"+self.recipe.pictureLocation)
         self.pic = tk.PhotoImage(file=os.path.dirname(os.path.realpath(__file__))+"/../res/pic/"+self.recipe.pictureLocation)
-
+        self.picLabel.config(image=self.pic)
 
         if self.recipe.isLocal:
             self.editButton.pack(side=tk.TOP,anchor="e") 
@@ -56,15 +68,15 @@ class RecipeFrame(Frame.Frame):
             self.favChoice.set(0)
 
         self.textContent['state'] = 'normal'
+
         self.textContent.delete('1.0','end')
 
-        self.textContent.image_create('end',image=self.pic)
-        self.textContent.insert('end',self.recipe.name+"\n\n",("header",))
+        self.textContent.insert('end',"\n"+self.recipe.name+"\n\n",("header",))
         
         self.textContent.insert('end',"Recette pour %i personne(s)"%self.recipe.nbrPeople+"\n\n")
 
         self.textContent.insert('end',"Ingredients :\n")
-        self.textContent.insert('end',recipe.ingredients+"\n","list")
+        self.textContent.insert('end',recipe.ingredients+"\n","list") 
 
         self.textContent.insert('end',"Réalisation\n\n",("header2"))
 
